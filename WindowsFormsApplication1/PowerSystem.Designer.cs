f296355b056cbbd627c0f2e2dede3246c30220cb
jeanskris <823509241@qq.com>
2016-10-07 11:11:43 +0800

    first commit

---
+++ WindowsFormsApplication1/PowerSystem.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.importData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.exportData = new System.Windows.Forms.Button();
+            this.printBtn = new System.Windows.Forms.Button();
+            this.manage = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.下方插入行ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除整行ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // importData
+            // 
+            this.importData.Location = new System.Drawing.Point(12, 39);
+            this.importData.Name = "importData";
+            this.importData.Size = new System.Drawing.Size(133, 54);
+            this.importData.TabIndex = 0;
+            this.importData.Text = "导入数据";
+            this.importData.UseVisualStyleBackColor = true;
+            this.importData.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 461);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // exportData
+            // 
+            this.exportData.Location = new System.Drawing.Point(218, 39);
+            this.exportData.Name = "exportData";
+            this.exportData.Size = new System.Drawing.Size(148, 54);
+            this.exportData.TabIndex = 2;
+            this.exportData.Text = "导出数据";
+            this.exportData.UseVisualStyleBackColor = true;
+            this.exportData.Click += new System.EventHandler(this.exportData_Click);
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(463, 39);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(158, 54);
+            this.printBtn.TabIndex = 3;
+            this.printBtn.Text = "一键打印";
+            this.printBtn.UseVisualStyleBackColor = true;
+            this.printBtn.Click += new System.EventHandler(this.printBtn_click);
+            // 
+            // manage
+            // 
+            this.manage.Location = new System.Drawing.Point(727, 50);
+            this.manage.Name = "manage";
+            this.manage.Size = new System.Drawing.Size(75, 23);
+            this.manage.TabIndex = 4;
+            this.manage.Text = "配置与管理";
+            this.manage.UseVisualStyleBackColor = true;
+            this.manage.Click += new System.EventHandler(this.manageBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(1, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1083, 501);
+            this.panel1.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.下方插入行ToolStripMenuItem,
+            this.删除整行ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 48);
+            // 
+            // 下方插入行ToolStripMenuItem
+            // 
+            this.下方插入行ToolStripMenuItem.Name = "下方插入行ToolStripMenuItem";
+            this.下方插入行ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.下方插入行ToolStripMenuItem.Text = "下方插入行";
+            // 
+            // 删除整行ToolStripMenuItem
+            // 
+            this.删除整行ToolStripMenuItem.Name = "删除整行ToolStripMenuItem";
+            this.删除整行ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.删除整行ToolStripMenuItem.Text = "删除整行";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 583);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.manage);
+            this.Controls.Add(this.printBtn);
+            this.Controls.Add(this.exportData);
+            this.Controls.Add(this.importData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button exportData;
+        private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.Button manage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 下方插入行ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除整行ToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/PowerSystem.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Data;
+using NPOI.SS.Util;
+using System.Collections.Generic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        DataTable data = new DataTable();
+        string filePath = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+             filePath = openFileDialog1.FileName;
+            string extension = Path.GetExtension(filePath);
+            dataGridView1.DataSource = ExcelInput(filePath, extension);
+
+        }
+       
+
+
+
+
+
+        private void exportData_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            saveFileDialog1.Filter = "xlsx|*.xlsx|xls files(*.xls)|*.xls|All files(*.*)|*.*";
+            saveFileDialog1.DefaultExt = "xlsx";
+            String fileSavePath = saveFileDialog1.FileName;
+            DataTableToExcel(data,fileSavePath);
+        }
+
+        private void printBtn_click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void manageBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void manage_click(object sender, EventArgs e)
+        {
+
+        }
+        //从excel读入数据到DataTable
+        public DataTable ExcelInput(string FilePath, string extension)
+        {
+            DataTable dataNew = new DataTable();
+            try
+            {               
+                FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite); ;
+                IWorkbook workbook = null;
+                ISheet sheet = null;
+                //根据路径通过已存在的excel来创建HSSFWorkbook，即整个excel文档
+                if (extension.Equals(".xlsx")) // 2007版本
+                    workbook = new XSSFWorkbook(fs);
+                else if (extension.Equals(".xls")) // 2003版本
+                    workbook = new HSSFWorkbook(fs);
+                try
+                {
+                    if (workbook != null)
+                    {
+                        sheet = workbook.GetSheetAt(0);
+                        sheet.ForceFormulaRecalculation = true;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.Message);
+
+                }
+
+                if (sheet != null)
+                {
+                    //获取Excel的最大行数和列数
+                    int rowsCount = sheet.PhysicalNumberOfRows;
+                    int colsCount = sheet.GetRow(0).PhysicalNumberOfCells;
+                    IRow firstRow = sheet.GetRow(0);
+                    int cellCount = firstRow.LastCellNum; //一行最后一个cell的编号 即总的列数
+                    dataNew.TableName = firstRow.GetCell(0).ToString();
+                    // sheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(0, 0, 0, cellCount));
+                    //CellRangeAddress（）该方法的参数次序是：开始行号，结束行号，开始列号，结束列号。
+                    List<String> addr = getMergedRegionValue(sheet, 3, 0);
+                    //add colums
+                    for (int i = firstRow.FirstCellNum; i < cellCount; i++)
+                    {
+                        ICell iCell = firstRow.GetCell(i);
+                        if (iCell != null)
+                        {
+                            DataColumn column = new DataColumn("" + iCell.ToString());
+                            //
+                            dataNew.Columns.Add(column);
+                        }
+
+                    }
+                    //read data
+                    for (int i = sheet.FirstRowNum; i <= rowsCount; ++i)
+                    {
+                        IRow row = sheet.GetRow(i);
+                        if (row == null) continue; //没有数据的行默认是null　　　　　　　
+
+                        DataRow dataRow = dataNew.NewRow();
+                        for (int j = row.FirstCellNum; j < cellCount; ++j)
+                        {
+                            ICell cell = row.GetCell(j);
+                            if (cell != null) //同理，没有数据的单元格都默认是null
+                            {
+                                if (cell.CellType == CellType.Formula) { dataRow[j] = cell.NumericCellValue.ToString("#0.000"); }//将公式计算结果显示出来
+                                else {
+                                    dataRow[j] = row.GetCell(j).ToString();
+                                }
+                            }
+
+                        }
+                        dataNew.Rows.Add(dataRow);
+                    }
+                }
+                sheet = null;
+                workbook = null;
+                data = dataNew;
+                return data;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                System.Windows.Forms.MessageBox.Show("Exception: " + ex.Message);
+                return null;
+            }
+           
+           
+        }
+        ///从DataTable读入数据到execel，NPOI 是开源的 POI 项目的.NET版，可以用来读写Excel，Word，PPT文件。在处理Excel文件上，NPOI 可以同时兼容 xls 和 xlsx。
+        /// 将DataTable数据导入到excel中
+        /// </summary>
+        /// <param name="data">要导入的数据</param>
+        /// <param name="isColumnWritten">DataTable的列名是否要导入</param>
+        /// <param name="sheetName">要导入的excel的sheet的名称</param>
+        /// <returns>导入数据行数(包含列名那一行)</returns>
+        public void DataTableToExcel(DataTable data, string fileName)
+        {
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            ISheet sheet = null;
+            IWorkbook workbook = null;
+            if (fileName != "")//中途取消
+            {
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                if (fileName.IndexOf(".xlsx") > 0) // 2007版本
+                    workbook = new XSSFWorkbook();
+                else if (fileName.IndexOf(".xls") > 0) // 2003版本
+                    workbook = new HSSFWorkbook();
+
+                try
+                {
+                    if (workbook != null)
+                    {
+                        sheet = workbook.CreateSheet("new");
+                        
+                    }
+
+                    IRow row = sheet.CreateRow(0);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Columns[j].ColumnName);
+                    }
+
+
+                    for (i = 0; i < data.Rows.Count; ++i)
+                    {
+                        IRow row1 = sheet.CreateRow(count);
+                        for (j = 0; j < data.Columns.Count; ++j)
+                        {
+                            row1.CreateCell(j).SetCellValue(data.Rows[i][j].ToString());
+                        }
+                        ++count;
+                    }
+                    workbook.Write(fs); //写入到excel               
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.Message);
+                    System.Windows.Forms.MessageBox.Show("Exception: " + ex.Message);
+                }
+            }
+        }
+        /**
+      * 获取合并单元格的值
+      * @param sheet 
+      * @param row 查找合并单元的起始行
+      * @param column 查找合并单元的起始列
+      * @return
+      */
+        public List<String> getMergedRegionValue(ISheet sheet, int row, int column)
+        {
+            int sheetMergeCount = sheet.NumMergedRegions;
+            List<String> address = new List<string>();
+
+            for (int i = 0; i < sheetMergeCount; i++)
+            {
+
+               CellRangeAddress ca = sheet.GetMergedRegion(i);
+                int firstColumn = ca.FirstColumn;
+                int lastColumn = ca.LastColumn;
+                int firstRow = ca.FirstRow;
+                int lastRow = ca.LastRow;
+
+                if (row >= firstRow && row <= lastRow)
+                {
+
+                    if (column >= firstColumn && column <= lastColumn)
+                    {
+                        IRow fRow = sheet.GetRow(firstRow);
+                        ICell fCell = fRow.GetCell(firstColumn);
+                        address.Add(fCell.StringCellValue);
+                        
+                    }
+                }
+            }
+
+            return address;
+        }
+
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+            dataGridView1.CurrentRow.Selected = true;
+          
+        }
+    }
+
+    }
